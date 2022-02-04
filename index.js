78d333221882c8a76c7c9dda35df1281f4b07955
Abhishek Sharma <abhishekpandit5599@gmail.com>
2022-02-05 03:21:53 +0530

    Backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 connectToMongo();
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -14,5 +14,5 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNoteBook app listening on port ${port}`)
 })
